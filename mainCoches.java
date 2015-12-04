df533afd36e40082af2eb119ac3587a0d8bf9f49
casoblus <casobrinolusquinos@gmail.com>
2015-12-04 02:07:32 +0100

    Da de ALTA() coches hasta que se completa el array o el usuario escribe fin.

--- mainCoches.java
+++ mainCoches.java
@@ -3,15 +3,26 @@ class mainCoches
 	public static void main(String[] args)
 	{
 		int tamano = 10;
+		String control;
+		int i = -1;
+
+		// Se crea un array "coche" de tamaño "tamano"
 		Coche[] coche = new coche[ tamano ];
 
-		for( i = 0; i < tamano; i++ )
-		{
+		do {
+
+			i++;
+
 			// Se crea un objeto coche
 			Coche coche[i] = new Coche();
 
 			// Se inicializan los atributos de coche[i]
 			coche[i].ALTA();
-		}
+
+			// Control para salir
+			System.out.println( "Escriba \"fin\" para terminar." );
+			control = entrada.nextLine();
+
+		} while( i < tamano - 1  || !control.equals("fin") );
 	}
 }
